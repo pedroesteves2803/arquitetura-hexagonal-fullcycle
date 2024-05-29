0796ddf92d266257a434effcaa69fdc9a71e7353
Pedro Esteves <pedroesteves2803@gmail.com>
2024-05-29 11:13:15 -0300

    Criar dto

---
+++ adapters/dto/product.go
@@ -0,0 +1,24 @@
+package dto
+
+type Product struct {
+	ID string `json:id`
+	Name string `json:name`
+	Price float64 `json:price`
+	Status string `json:status`
+}
+
+func (p *Product) Bind(product *application.Product, error) {
+	if(p.ID != "") {
+		product.ID = p.ID
+	}
+
+	product.Name = p.Name
+	product.Price = p.Price
+	product.Status = P.Status
+	_, err := product.IsValid()
+	if err != nil {
+		return &application.Product{}, err
+	}
+
+	return product, nil
+}
